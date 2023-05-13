18e7d0647689b18cd40041af33c0fc9b35fb969e
Javier <javcomas@ucm.es>
2023-05-13 16:59:36 +0200

    base procedural terminada

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -16,9 +16,11 @@ public class MapGenerator : MonoBehaviour
     public DrawMode drawMode;
 
     public int mapSize;
-    public float HeightPerBlock = 0.5f;
-    public float noiseScale;
+    const int chunkSize = 50;
 
+    public float HeightPerBlock = 0.5f;
+
+    public float noiseScale;
     public int octaves;
     [Range(0f, 1f)]
     public float persistance;
@@ -32,21 +34,17 @@ public class MapGenerator : MonoBehaviour
     public bool useFallOff = false;
     public bool autoUpdate = false;
 
-    public GameObject basee;
-    public GameObject seaBottom;
-    public GameObject Edges;
-    float[,] noiseMap;
-    float[,] fallOffMap;
     Cell[,] mapaCells;
+    Dictionary<Vector2, Chunk> map3D= new Dictionary<Vector2, Chunk>();
     private void Awake(){
         GenerateMap();
     }
     public void GenerateMap(){
-        fallOffMap = Noise.GenerateFalloffMap(mapSize);
-        noiseMap= Noise.GenerateNoiseMap(mapSize, mapSize,seed,noiseScale,octaves,persistance,lacunarity,offset);
+        float[,] fallOffMap = Noise.GenerateFalloffMap(mapSize);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapSize, mapSize,seed,noiseScale,octaves,persistance,lacunarity,offset);
+        mapaCells = new Cell[mapSize, mapSize];
 
-        Color[] colorMap = new Color[mapSize * mapSize];
-        mapaCells= new Cell[mapSize,mapSize];
+        Color[] colorMap = new Color[mapSize * mapSize];        
         for (int y = 0; y < mapSize; y++){
             for (int x = 0; x < mapSize; x++){
                 if(useFallOff) noiseMap[x,y]=Mathf.Clamp01( noiseMap[x,y] - fallOffMap[x,y]);// calculo el nue o noise con respecto al falloff
@@ -68,10 +66,8 @@ public class MapGenerator : MonoBehaviour
         if (drawMode == DrawMode.NoiseMap) display.DrawTextureMap(TextureGenerator.TextureFromNoiseMap(noiseMap));
         else if (drawMode == DrawMode.ColorMap) { 
             display.DrawTextureMap(TextureGenerator.TextureFromColorMap(colorMap, mapSize, mapSize));
-            MeshGenerator.GenerateTerrainMesh(mapaCells,basee,HeightPerBlock,false);
-            MeshGenerator.DrawEdges(mapaCells, Edges, HeightPerBlock, false);
-            //MeshGenerator.GenerateTerrainMesh(mapaCells,seaBottom,HeightPerBlock,true);          
-            
+            map3D.Clear();
+            GenerarMapaPorChunks();
         }
         else if (drawMode == DrawMode.FallOff) display.DrawTextureMap(TextureGenerator.TextureFromNoiseMap(Noise.GenerateFalloffMap(mapSize)));
     }
@@ -80,5 +76,55 @@ public class MapGenerator : MonoBehaviour
         if(mapSize < 1) mapSize = 1;
         if(lacunarity<1) lacunarity=1;
         if(octaves< 0) octaves=0;
+    }
+    void GenerarMapaPorChunks(){
+        int numChunks = mapSize / chunkSize;
+        if (mapSize % chunkSize != 0) numChunks++;
+
+        for (int y = 0; y < numChunks; y++){
+            for (int x = 0; x < numChunks; x++){
+                Vector2 chunkPos= new Vector2(x, y);
+                if(!map3D.ContainsKey(chunkPos)){
+                    map3D[chunkPos] = new Chunk(chunkPos,mapaCells,HeightPerBlock,transform);
+                }
+                else{
+                    map3D[chunkPos].GenerateTerrainMesh(mapaCells, HeightPerBlock);
+                }
+            }
+        }
+
+    }    
+}
+public class Chunk{
+    public Vector2 posMap;
+    GameObject suelo;
+    GameObject edges;
+    public Chunk(Vector2 posMap, Cell[,] mapaCells, float HeightPerBlock,Transform parent){
+        this.posMap = posMap;
+
+        suelo = new GameObject("Suelo " + posMap);
+        edges = new GameObject("Edges " + posMap);
+        setParent(parent);
+        edges.transform.SetParent(suelo.transform);             
+
+        Material sueloMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+        suelo.AddComponent<MeshFilter>();
+        suelo.AddComponent<MeshRenderer>().material=sueloMaterial;
+
+        Material edgesMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+        edges.AddComponent<MeshFilter>();
+        edges.AddComponent<MeshRenderer>().material=edgesMaterial;
+
+        GenerateTerrainMesh(mapaCells, HeightPerBlock);
+
+        suelo.AddComponent<MeshCollider>();
+        edges.AddComponent<MeshCollider>();
+    }
+    public void GenerateTerrainMesh(Cell[,] mapaCells, float heightPerBlock){
+        MeshGenerator.GenerateTerrainMeshChunk(mapaCells,posMap, suelo, heightPerBlock);
+        MeshGenerator.DrawEdgesChunk(mapaCells,posMap, edges, heightPerBlock);
+    }
+    public void setParent(Transform parent){
+        suelo.transform.SetParent(parent);
     }
 }
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,20 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using UnityEditor.Hardware;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public static class MeshGenerator{
-    static bool createVert(bool seaBottom, float height)
+    const int chunkSize = 50;
+    public static void GenerateTerrainMeshChunk(Cell[,] mapaCells, Vector2 pos, GameObject basee, float heightPerBlock)
     {
-        if (seaBottom && height < 0.4f) return true;
-        else if (!seaBottom && height > 0.4f) return true;
-        return false;
-    }
-    public static void GenerateTerrainMesh(Cell[,] mapaCells,GameObject basee,float heightPerBlock,bool seaBottom){
         int size = mapaCells.GetLength(0);
-       
+
         float topLeftX = (size - 1) / -2f;
         float topLeftZ = (size - 1) / -2f;
 
@@ -23,31 +16,33 @@ public static class MeshGenerator{
         List<int> triangles = new List<int>();
         List<Vector2> uvs = new List<Vector2>(); //coordenadas de textura
 
-        for (int y = 0; y < size; y++){
-            for (int x = 0; x < size; x++){
+        for (int y = (int)pos.y * chunkSize; y < (int)(pos.y + 1) * chunkSize && y < size; y++)
+        {
+            for (int x = (int)pos.x*chunkSize; x < (int)(pos.x+1)*chunkSize && x<size; x++)
+            {
                 Cell cell = mapaCells[x, y];
-               // if (createVert(seaBottom,cell.type.height)){
-                    float currHeight = heightPerBlock * cell.noise *100;
-                    //definir los vertices de la celda
-                    Vector3 a = new Vector3(topLeftX + x - 0.5f, currHeight, topLeftZ - y + 0.5f);
-                    Vector3 b = new Vector3(topLeftX + x + 0.5f, currHeight, topLeftZ - y + 0.5f);
-                    Vector3 c = new Vector3(topLeftX + x - 0.5f, currHeight, topLeftZ - y - 0.5f);
-                    Vector3 d = new Vector3(topLeftX + x + 0.5f, currHeight, topLeftZ - y - 0.5f);
 
-                    Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
-                    Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
-                    Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
-                    Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+                float currHeight = heightPerBlock * cell.noise * 100;
+                //definir los vertices de la celda
+                Vector3 a = new Vector3(topLeftX + x - 0.5f, currHeight, topLeftZ - y + 0.5f);
+                Vector3 b = new Vector3(topLeftX + x + 0.5f, currHeight, topLeftZ - y + 0.5f);
+                Vector3 c = new Vector3(topLeftX + x - 0.5f, currHeight, topLeftZ - y - 0.5f);
+                Vector3 d = new Vector3(topLeftX + x + 0.5f, currHeight, topLeftZ - y - 0.5f);
 
-                    Vector3[] v = new Vector3[] { a, b, c, b, d, c };
-                    Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
-                    for (int k = 0; k < 6; k++) //crear los triangulos 
-                    {
-                        vertices.Add(v[k]);
-                        triangles.Add(triangles.Count);
-                        uvs.Add(uv[k]);
-                    }
-                //}
+                Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
+                Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
+                Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
+                Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+
+                Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
+                for (int k = 0; k < 6; k++) //crear los triangulos 
+                {
+                    vertices.Add(v[k]);
+                    triangles.Add(triangles.Count);
+                    uvs.Add(uv[k]);
+                }
+
             }
         }
 
@@ -58,28 +53,29 @@ public static class MeshGenerator{
 
         MeshFilter meshFilter = basee.GetComponent<MeshFilter>();//Renderizar la malla
         meshFilter.mesh = BaseMesh;
-    
-        var renderer = basee.GetComponent<MeshRenderer>();
-        DrawTexture(mapaCells, renderer, seaBottom); 
-        
-    }
 
-    public static void DrawTexture(Cell[,] mapaCells,MeshRenderer renderer,bool seaBottom){
+        var renderer = basee.GetComponent<MeshRenderer>();
+        DrawTextureChunk(mapaCells,pos,renderer);
+
+    }
+    public static void DrawTextureChunk(Cell[,] mapaCells, Vector2 pos, MeshRenderer renderer)
+    {
         int size = mapaCells.GetLength(0);
 
         Texture2D texture = new Texture2D(size, size);
-        for (int y = 0; y < size; y++){
-            for (int x = 0; x < size; x++){
-                Cell cell = mapaCells[x, y]; 
-                //if(createVert(seaBottom,cell.type.height))
-                 texture.SetPixel(x, y, cell.type.color);                               
+        for (int y = (int)pos.y * chunkSize; y < (int)(pos.y + 1) * chunkSize && y < size; y++)
+        {
+            for (int x = (int)pos.x * chunkSize; x < (int)(pos.x + 1) * chunkSize && x < size; x++)
+            {
+                Cell cell = mapaCells[x, y];
+                texture.SetPixel(x, y, cell.type.color);
             }
         }
         texture.filterMode = FilterMode.Point;
         texture.Apply();
         renderer.sharedMaterial.mainTexture = texture;
     }
-    public static void DrawEdges(Cell[,] mapaCells,GameObject edges, float heightPerBlock, bool seaBottom)
+    public static void DrawEdgesChunk(Cell[,] mapaCells, Vector2 pos, GameObject edges, float heightPerBlock)
     {
         int size = mapaCells.GetLength(0);
 
@@ -90,107 +86,119 @@ public static class MeshGenerator{
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
         List<Vector2> uvs = new List<Vector2>(); //coordenadas de textura
-        for (int y = 0; y < size; y++){
-            for (int x = 0; x < size; x++){
+        for (int y = (int)pos.y * chunkSize; y < (int)(pos.y + 1) * chunkSize && y < size; y++)
+        {
+            for (int x = (int)pos.x * chunkSize; x < (int)(pos.x + 1) * chunkSize && x < size; x++)
+            {
                 Cell cell = mapaCells[x, y];
                 float currHeight = heightPerBlock * cell.noise * 100;
-                //if (createVert(false,cell.noise)){
-                    if (x > 0){
-                        Cell left = mapaCells[x - 1, y];//izquierda
-                        if (left.noise<cell.noise){
-                            float leftHeight = currHeight - (heightPerBlock * left.noise * 100);
-                            Vector3 a = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y + .5f);
-                            Vector3 b = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y - .5f);
-                            Vector3 c = new Vector3(topLeftX + x - .5f, currHeight - leftHeight, topLeftZ - y + .5f);
-                            Vector3 d = new Vector3(topLeftX + x - .5f, currHeight - leftHeight, topLeftZ - y - .5f);
 
-                            Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
-                            Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
-                            Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
-                            Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+                if (x > 0)
+                {
+                    Cell left = mapaCells[x - 1, y];//izquierda
+                    if (left.noise < cell.noise)
+                    {
+                        float leftHeight = currHeight - (heightPerBlock * left.noise * 100);
+                        Vector3 a = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y + .5f);
+                        Vector3 b = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y - .5f);
+                        Vector3 c = new Vector3(topLeftX + x - .5f, currHeight - leftHeight, topLeftZ - y + .5f);
+                        Vector3 d = new Vector3(topLeftX + x - .5f, currHeight - leftHeight, topLeftZ - y - .5f);
 
-                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
-                            Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
-                            for (int k = 0; k < 6; k++){
-                                vertices.Add(v[k]);
-                                triangles.Add(triangles.Count);
-                                uvs.Add(uv[k]);
-                            }
-                            
+                        Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
+                        Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
+                        Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
+                        Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+
+                        Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                        Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
+                        for (int k = 0; k < 6; k++)
+                        {
+                            vertices.Add(v[k]);
+                            triangles.Add(triangles.Count);
+                            uvs.Add(uv[k]);
                         }
+
                     }
-                    if (x < size - 1){
-                        Cell right = mapaCells[x + 1, y];//derecha
-                        if (right.noise<cell.noise){
-                            float rightHeight = currHeight - (heightPerBlock * right.noise * 100);
-                            Vector3 a = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y - .5f);
-                            Vector3 b = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y + .5f);
-                            Vector3 c = new Vector3(topLeftX + x + .5f, currHeight-rightHeight, topLeftZ - y - .5f);
-                            Vector3 d = new Vector3(topLeftX + x + .5f, currHeight-rightHeight, topLeftZ - y + .5f);
+                }
+                if (x < size - 1)
+                {
+                    Cell right = mapaCells[x + 1, y];//derecha
+                    if (right.noise < cell.noise)
+                    {
+                        float rightHeight = currHeight - (heightPerBlock * right.noise * 100);
+                        Vector3 a = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y - .5f);
+                        Vector3 b = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y + .5f);
+                        Vector3 c = new Vector3(topLeftX + x + .5f, currHeight - rightHeight, topLeftZ - y - .5f);
+                        Vector3 d = new Vector3(topLeftX + x + .5f, currHeight - rightHeight, topLeftZ - y + .5f);
 
-                            Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
-                            Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
-                            Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
-                            Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+                        Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
+                        Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
+                        Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
+                        Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
 
-                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
-                            Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
-                            for (int k = 0; k < 6; k++){
-                                vertices.Add(v[k]);
-                                triangles.Add(triangles.Count);
-                                uvs.Add(uv[k]);
-                            }
-                           
+                        Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                        Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
+                        for (int k = 0; k < 6; k++)
+                        {
+                            vertices.Add(v[k]);
+                            triangles.Add(triangles.Count);
+                            uvs.Add(uv[k]);
                         }
+
                     }
-                    if (y > 0){
-                        Cell down = mapaCells[x, y - 1];//abajo
-                        if (down.noise< cell.noise){ 
-                            float downHeight = currHeight - (heightPerBlock * down.noise * 100);
-                            Vector3 a = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y + .5f);
-                            Vector3 b = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y + .5f);
-                            Vector3 c = new Vector3(topLeftX + x - .5f, currHeight-downHeight, topLeftZ - y + .5f);
-                            Vector3 d = new Vector3(topLeftX + x + .5f, currHeight-downHeight, topLeftZ - y + .5f);
+                }
+                if (y > 0)
+                {
+                    Cell down = mapaCells[x, y - 1];//abajo
+                    if (down.noise < cell.noise)
+                    {
+                        float downHeight = currHeight - (heightPerBlock * down.noise * 100);
+                        Vector3 a = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y + .5f);
+                        Vector3 b = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y + .5f);
+                        Vector3 c = new Vector3(topLeftX + x - .5f, currHeight - downHeight, topLeftZ - y + .5f);
+                        Vector3 d = new Vector3(topLeftX + x + .5f, currHeight - downHeight, topLeftZ - y + .5f);
 
-                            Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
-                            Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
-                            Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
-                            Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+                        Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
+                        Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
+                        Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
+                        Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
 
-                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
-                            Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
-                            for (int k = 5; k >= 0; k--){
-                                vertices.Add(v[k]);
-                                triangles.Add(triangles.Count);
-                                uvs.Add(uv[k]);
-                            }
-                            
+                        Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                        Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
+                        for (int k = 5; k >= 0; k--)
+                        {// INSERTARLOS AL REVES PQ SI NO SE INVIERTEN LAS NORMALES
+                            vertices.Add(v[k]);
+                            triangles.Add(triangles.Count);
+                            uvs.Add(uv[k]);
                         }
+
                     }
-                    if (y < size - 1){
-                        Cell up = mapaCells[x, y + 1];//arriba
-                        if (up.noise< cell.noise){
-                            float upHeight = currHeight - (heightPerBlock * up.noise * 100);
-                            Vector3 a = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y - .5f);
-                            Vector3 b = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y - .5f);
-                            Vector3 c = new Vector3(topLeftX + x + .5f, currHeight - upHeight, topLeftZ - y - .5f);
-                            Vector3 d = new Vector3(topLeftX + x - .5f, currHeight - upHeight, topLeftZ - y - .5f);
+                }
+                if (y < size - 1)
+                {
+                    Cell up = mapaCells[x, y + 1];//arriba
+                    if (up.noise < cell.noise)
+                    {
+                        float upHeight = currHeight - (heightPerBlock * up.noise * 100);
+                        Vector3 a = new Vector3(topLeftX + x + .5f, currHeight, topLeftZ - y - .5f);
+                        Vector3 b = new Vector3(topLeftX + x - .5f, currHeight, topLeftZ - y - .5f);
+                        Vector3 c = new Vector3(topLeftX + x + .5f, currHeight - upHeight, topLeftZ - y - .5f);
+                        Vector3 d = new Vector3(topLeftX + x - .5f, currHeight - upHeight, topLeftZ - y - .5f);
 
-                            Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
-                            Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
-                            Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
-                            Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+                        Vector2 uvA = new Vector2(x / (float)size, y / (float)size); //definir coordenadas de textura correspondientes a cada v
+                        Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
+                        Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
+                        Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
 
-                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
-                            Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
-                            for (int k = 5; k >= 0; k--){
-                                vertices.Add(v[k]);
-                                triangles.Add(triangles.Count);
-                                uvs.Add(uv[k]);
-                            }
+                        Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                        Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
+                        for (int k = 5; k >= 0; k--)
+                        {// INSERTARLOS AL REVES PQ SI NO SE INVIERTEN LAS NORMALES
+                            vertices.Add(v[k]);
+                            triangles.Add(triangles.Count);
+                            uvs.Add(uv[k]);
                         }
-                        
-                    //}
+                    }                    
                 }
             }
         }
@@ -203,6 +211,6 @@ public static class MeshGenerator{
         meshFilter.mesh = mesh;
 
         var render = edges.GetComponent<MeshRenderer>();
-        DrawTexture(mapaCells, render,false);
-    }   
+        DrawTextureChunk(mapaCells,pos,render);
+    }
 }
